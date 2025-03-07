2be5e2986174297d55e9d32cdc46e92d130a696f
sunghyun <specialdays227@gamil.com>
2025-03-07 18:31:25 +0900

    # < type > : 250307-UDIGO-feat/select-by
    # 박성현
    
    ################
    
    # 250307 - feat/select-by 올립니다.
    
    ################

--- src/main/java/com/ohgiraffers/udigo/crud/menu/controller/MenuController.java
+++ src/main/java/com/ohgiraffers/udigo/crud/menu/controller/MenuController.java
@@ -1,12 +1,47 @@
 package com.ohgiraffers.udigo.crud.menu.controller;
 
 
+import com.ohgiraffers.udigo.crud.menu.model.dto.MenuDTO;
+import com.ohgiraffers.udigo.crud.menu.model.service.MenuService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/menu")
 public class MenuController {
+
+    private static final Logger logger = LogManager.getLogger(MenuController.class);
+
+    private final MenuService menuService;
+    private final MessageSource messageSource;
+
+    @Autowired
+    public MenuController(MenuService menuService, MessageSource messageSource) {
+
+        this.menuService = menuService;
+        this.messageSource = messageSource;
+
+    }
+
+
+    @GetMapping("/detail/{code}")
+    public String findMenuDetail(@PathVariable("code") int code,
+                                 Model model) {
+
+        MenuDTO menu = menuService.findMenuByCode(code);
+
+        model.addAttribute("menu", menu);
+
+        return "menu/detail";
+    }
+
 
 
 
--- src/main/java/com/ohgiraffers/udigo/crud/menu/model/service/MenuService.java
+++ src/main/java/com/ohgiraffers/udigo/crud/menu/model/service/MenuService.java
@@ -1,11 +1,25 @@
 package com.ohgiraffers.udigo.crud.menu.model.service;
 
+import com.ohgiraffers.udigo.crud.menu.model.dao.MenuMapper;
+import com.ohgiraffers.udigo.crud.menu.model.dto.MenuDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MenuService {
 
+    private final MenuMapper menuMapper;
+
+    @Autowired
+    public MenuService(MenuMapper menuMapper) {
+        this.menuMapper = menuMapper;
+    }
 
 
+
+
+    public MenuDTO findMenuByCode(int code) {
+        return menuMapper.findMenuByCode(code);
+    }
 
 }
