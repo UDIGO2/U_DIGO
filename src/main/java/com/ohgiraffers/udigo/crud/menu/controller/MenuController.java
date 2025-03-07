cb4388a98b5bf51400bf18990cef51c05a9445b0
heo-ar <heohehe3@gmail.com>
2025-03-07 18:32:01 +0900

    # < type > : 250307-UDIGO-feat/selectall
    # 허아름
    
    ################
    
    # 250307 - feat/selectall 올립니다.
    
    ################

--- src/main/java/com/ohgiraffers/udigo/crud/menu/controller/MenuController.java
+++ src/main/java/com/ohgiraffers/udigo/crud/menu/controller/MenuController.java
@@ -1,14 +1,45 @@
 package com.ohgiraffers.udigo.crud.menu.controller;
 
 
+import com.ohgiraffers.udigo.crud.menu.model.dto.MenuDTO;
+import com.ohgiraffers.udigo.crud.menu.model.service.MenuService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/menu")
 public class MenuController {
 
+    private static final Logger logger = LogManager.getLogger(MenuController.class);
 
+    private final MenuService menuService;
+    private final MessageSource messageSource;
+
+    @Autowired
+    public MenuController(MenuService menuService, MessageSource messageSource) {
+
+        this.menuService = menuService;
+        this.messageSource = messageSource;
+
+    }
+
+    @GetMapping("/list")
+    public String findMenuList(Model model) {
+
+        List<MenuDTO> menuList = menuService.findAllMenu();
+
+        model.addAttribute("menuList", menuList);
+
+        return "menu/list";
+    }
 
 
 }
--- src/main/java/com/ohgiraffers/udigo/crud/menu/model/service/MenuService.java
+++ src/main/java/com/ohgiraffers/udigo/crud/menu/model/service/MenuService.java
@@ -1,11 +1,30 @@
 package com.ohgiraffers.udigo.crud.menu.model.service;
 
+import com.ohgiraffers.udigo.crud.menu.model.dao.MenuMapper;
+import com.ohgiraffers.udigo.crud.menu.model.dto.CategoryDTO;
+import com.ohgiraffers.udigo.crud.menu.model.dto.MenuDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MenuService {
 
 
+    private final MenuMapper menuMapper;
 
+    @Autowired
+    public MenuService(MenuMapper menuMapper) {
+        this.menuMapper = menuMapper;
+    }
+
+    public List<MenuDTO> findAllMenu() {
+        return menuMapper.findAllMenu();
+    }
+
+    public List<CategoryDTO> findAllCategory() {
+        return menuMapper.findAllCategory();
+    }
 
 }
