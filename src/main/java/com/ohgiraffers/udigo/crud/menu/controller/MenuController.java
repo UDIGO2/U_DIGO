60e20f7ebeb21810e705059e7960ff2ea2e32768
park-songyi0718 <lucky0052@naver.com>
2025-03-07 18:28:22 +0900

    # < type > : 250307-UDIGO-feat/delete
    # 박송이
    
    ################
    
    # 250307 - feat/delete 올립니다.
    
    ################

--- src/main/java/com/ohgiraffers/udigo/crud/menu/controller/MenuController.java
+++ src/main/java/com/ohgiraffers/udigo/crud/menu/controller/MenuController.java
@@ -1,14 +1,43 @@
 package com.ohgiraffers.udigo.crud.menu.controller;
 
 
+import com.ohgiraffers.udigo.crud.menu.model.service.MenuService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/menu")
 public class MenuController {
 
+    private static final Logger logger = LogManager.getLogger(MenuController.class);
 
+    private final MenuService menuService;
+    private final MessageSource messageSource;
 
+    @Autowired
+    public MenuController(MenuService menuService, MessageSource messageSource) {
+
+        this.menuService = menuService;
+        this.messageSource = messageSource;
+
+    }
+
+    @PostMapping("/delete/{code}")
+    public String deleteMenu(@PathVariable("code") int code,
+                             RedirectAttributes rAttr) {
+
+        menuService.deleteMenu(code);
+
+        rAttr.addFlashAttribute("successMessage", "메뉴가 성공적으로 삭제되었습니다.");
+
+        return "redirect:/menu/list";
+    }
 
 }
--- src/main/java/com/ohgiraffers/udigo/crud/menu/model/service/MenuService.java
+++ src/main/java/com/ohgiraffers/udigo/crud/menu/model/service/MenuService.java
@@ -1,11 +1,23 @@
 package com.ohgiraffers.udigo.crud.menu.model.service;
 
+import com.ohgiraffers.udigo.crud.menu.model.dao.MenuMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MenuService {
 
+    private final MenuMapper menuMapper;
+    @Autowired
+    public MenuService(MenuMapper menuMapper) {
+        this.menuMapper = menuMapper;
+    }
 
-
-
+    @Transactional
+    public void deleteMenu(int code) {
+        menuMapper.deleteMenu(code);
+    }
 }
+
+
